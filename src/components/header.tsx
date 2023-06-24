5955154d57c238b307d5844a2df24e393b2a1db9
Jamie Stegman <hello@jamiestegman.com>
2023-06-24 19:33:00 +1000

    Introduce shadcn/ui with theme toggle. Build barebones homepage hero.

---
+++ src/components/header.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { ThemeToggle } from "./theme/theme-toggle";
+import { buttonVariants } from "./ui/button";
+
+export const Header = () => {
+    return (
+        <div className="supports-backdrop-blur:bg-background/60 sticky top-0 mb-4 flex h-14 w-full items-center justify-between border-zinc-200 bg-background/95 backdrop-blur">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Avatar>
+                            <AvatarImage src="" />
+                            <AvatarFallback>RT</AvatarFallback>
+                        </Avatar>
+                    </NavigationMenuItem>
+
+                    <NavigationMenuItem>
+                        <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
+                                Pricing
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+
+            <div className="flex space-x-2">
+                <Link
+                    className={buttonVariants({ variant: "ghost" })}
+                    href="/login"
+                >
+                    Log in
+                </Link>
+                <Link
+                    className={buttonVariants({ variant: "default" })}
+                    href="/signup"
+                >
+                    Sign up
+                </Link>
+            </div>
+        </div>
+    );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ThemeProvider } from '@/components/theme/theme-provider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
