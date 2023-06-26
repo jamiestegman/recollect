e69f8949fb3ce05e939d389e7a7ca2b683d2dda7
Jamie Stegman <hello@jamiestegman.com>
2023-06-26 17:12:12 +1000

    Add x-frame-options headers to routes in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-}
+    reactStrictMode: false,
+    async headers() {
+        return [
+            {
+                source: "/login",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                ],
+            },
+            {
+                source: "/signup",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                ],
+            },
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "Referrer-Policy",
+                        value: "no-referrer-when-downgrade",
+                    },
+                    {
+                        key: "X-DNS-Prefetch-Control",
+                        value: "on",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
